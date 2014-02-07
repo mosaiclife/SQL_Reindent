2f7129357fa2a69454d3917861ba4af9ccc67007
hongpyo <mosaiclife@naver.com>
2014-02-07 14:54:50 +0900

    Delete Stack where in Tree.py

--- python/Tree.py
+++ python/Tree.py
@@ -1,43 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import sys
-
-# 스택
-class Stack():
-    
-    def __init__(self):
-        self.top = -1
-        self.stack = []
-    
-    def pop(self):
-        if self.isEmpty() == 1:
-            print 'Stack is Empty!'
-            sys.exit()
-        else:
-            temp = self.stack[self.top]
-            self.top -= 1
-            return temp
-    
-    def push(self, data):
-        if self.isOverflow() == 1:
-            print 'Stack Over Flow!'
-            sys.exit()
-        self.top += 1
-        self.stack.append(data)
-        return 0
-    
-    def isOverflow(self):
-        if self.top >= len(self.stack):
-            return 1
-        else:
-            return 0
-    
-    def isEmpty(self):
-        if self.top < 0:
-            return 1
-        else:
-            return 0
-
 
 # 이진 트리
 class Tree():
